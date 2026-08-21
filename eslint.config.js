b5e686406e4dcfa3498aff5a1dca881229a297be
Luong Nguyen <luongnv89@gmail.com>
2026-08-21 17:01:06 +0000

    refactor(deps): migrate to ESLint 10 flat config (#25)

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const reactHooks = require('eslint-plugin-react-hooks');
+const reactRefresh = require('eslint-plugin-react-refresh');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  {
+    ignores: [
+      'dist',
+      'dist-bak',
+      'eslint.config.js',
+      '**/*.config.js',
+      '**/*.config.cjs',
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'no-constant-binary-expression': 'off',
+      'no-useless-assignment': 'off',
+      'preserve-caught-error': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }
+);
